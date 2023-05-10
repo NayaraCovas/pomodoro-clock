73e08d1ca11da9a6c6b93a593d1645aa5bb4cf23
Nayara Covas <nayaracovas1@outlook.com>
2023-05-10 09:17:21 +0200

    My commit

---
+++ main.js
@@ -0,0 +1,107 @@
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var reset = document.getElementById("reset");
+
+var wm = document.getElementById("w_minutes");
+var ws = document.getElementById("w_seconds");
+
+var bm = document.getElementById("b_minutes");
+var bs = document.getElementById("b_seconds");
+
+var minusb = document.getElementById("work-minus-button");
+var plusb = document.getElementById("work-plus-button");
+var minusBreak = document.getElementById("break-minus-button");
+var plusBreak = document.getElementById("break-plus-button");
+
+//Adding minus and plus buttons funcionality to WORK:
+
+minusb.addEventListener("click", function () {
+  if (wm.innerText > 0) {
+    document.getElementById("w_minutes").innerText--;
+  }
+});
+
+plusb.addEventListener("click", function () {
+  document.getElementById("w_minutes").innerText++;
+});
+
+//Adding minus and plus buttons funcionality to BREAK:
+
+minusBreak.addEventListener("click", function () {
+  if (bm.innerText > 0) {
+    document.getElementById("b_minutes").innerText--;
+  }
+});
+
+plusBreak.addEventListener("click", function () {
+  document.getElementById("b_minutes").innerText++;
+});
+
+//store reference to a timer variable
+var startTimer;
+
+start.addEventListener("click", function () {
+  if (startTimer === undefined) {
+    startTimer = setInterval(timer, 1000);
+  } else {
+    alert("Timer is already running");
+  }
+});
+
+reset.addEventListener("click", function () {
+  wm.innerText = 25;
+  ws.innerText = "00";
+
+  bm.innerText = 5;
+  bs.innerText = "00";
+
+  document.getElementById("counter").innerText = 0;
+  stopInterval();
+  startTimer = undefined;
+});
+
+stop.addEventListener("click", function () {
+  stopInterval();
+  startTimer = undefined;
+});
+
+//Start Timer Function
+function timer() {
+  //Work Timer Countdown
+  if (ws.innerText != 0) {
+    ws.innerText--;
+  } else if (wm.innerText != 0 && ws.innerText == 0) {
+    ws.innerText = 59;
+    wm.innerText--;
+  }
+  //Break Timer Countdown
+  if (wm.innerText == 0 && ws.innerText == 0) {
+    if (bs.innerText != 0) {
+      bs.innerText--;
+    } else if (bm.innerText != 0 && bs.innerText == 0) {
+      bs.innerText = 59;
+      bm.innerText--;
+    }
+  }
+
+  //Increment Counter by one if one full cycle is completed
+  if (
+    wm.innerText == 0 &&
+    ws.innerText &&
+    bm.innerText == 0 &&
+    bs.innerText == 0
+  ) {
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById("counter").innerText++;
+  }
+}
+
+// Stop Timer Function()
+function stopInterval() {
+  clearInterval(startTimer);
+}
